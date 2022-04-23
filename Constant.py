8cd28782ef1a0d0b2d0f904bad9eb2175e622585
svc.hcx-automation <guot@guot-a02.vmware.com>
2022-04-23 16:36:43 -0700

    first upload

--- Constant.py
+++ Constant.py
@@ -1,7 +1,14 @@
 RETRY = 30
+RETRY_CLEANUP = 3
+#for HCM and HCX cloud
 BASE_HCX_API = "/hybridity/api"
-BASE_HCX_SAAS_API = "/api/v1"
 HCX_SESSION = BASE_HCX_API+"/sessions"
-HCX_SAAS_SITE = BASE_HCX_API+"/saas/site"
+HCX_SAAS_ADAPTER_SITE = BASE_HCX_API+"/saas/site"
+
+#for HCXSaas portal
+BASE_HCX_SAAS_API = "/api/v1"
+HCXSAAS_SRV_ONBOARD_REQ = BASE_HCX_SAAS_API+"/site-onboarding/request-registration"
+
+#For csp authotication
 CSP_TOKEN_URI="/csp/gateway/am/api/auth/api-tokens/authorize?refresh_token=%s"
 CSP_URL="console-stg.cloud.vmware.com"
