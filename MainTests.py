c054e529ecdf0f5d27b9986a159c7980397b344e
svc.hcx-automation <guot@guot-a02.vmware.com>
2022-04-24 17:20:41 -0700

    first upload

---
+++ MainTests.py
@@ -0,0 +1,50 @@
+import unittest
+
+import lib.HTMLTestRunner
+
+from test.suites.PreflightTest import PreflightTest
+from test.suites.SitePairsTest import SitePairsTest
+from test.suites.NetworkExtensionsTest import NetworkExtensionsTest
+from test.suites.HCXSaasAdapterTest import HCXSaasAdapaterTest
+
+
+def get_attrs():
+    g2attrs = [
+        ('Project Name', 'HCX Saas Automation'),
+        ('Responsible Team', 'HCX Saas Team'),
+        ('Build Number', '42'),
+
+    ]
+    g3attrs = [
+        ('Produc Under Test', 'HCX Saas adapter test Suite'),
+        ('Product Team', 'HCX Saas Team')
+    ]
+
+    attrs = {"group2": g2attrs, "group3": g3attrs}
+    return attrs
+
+
+def get_description():
+    des = "HCX Saas Automation"
+    return des
+
+if __name__ == '__main__':
+    # Create the report file
+    html_report = open('report/test_report.html', 'wb')
+    # Create the runner and set the file as output and higher verbosity
+    runner = lib.HTMLTestRunner.HTMLTestRunner(stream=html_report, verbosity=2, attrs=get_attrs(),
+                                               description=get_description())
+    # Create a test list
+    tests = [
+        PreflightTest, HCXSaasAdapaterTest
+    ]
+    # Load test cases
+    loader = unittest.TestLoader()
+    # Create a SuiteCase
+    test_list = []
+    for test in tests:
+        cases = loader.loadTestsFromTestCase(test)
+        test_list.append(cases)
+    suite = unittest.TestSuite(test_list)
+    # Run the suite
+    runner.run(suite)
