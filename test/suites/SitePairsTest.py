adefd9134c82b86384c97180e493188d60184345
svc.hcx-automation <guot@guot-a02.vmware.com>
2022-04-24 17:02:32 -0700

    first upload

---
+++ test/suites/SitePairsTest.py
@@ -0,0 +1,26 @@
+import json
+import os
+import ssl
+import sys
+import logging
+import time
+import unittest
+import lib.HTMLTestRunner as HTMLTestRunner
+
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), '../testdata/testdata.json')
+TESTBED_FILENAME = os.path.join(os.path.dirname(__file__), '../testdata/testbed_info.json')
+
+"""Set up local logger"""
+logger = logging.getLogger(os.path.basename(__file__))
+logger.setLevel(logging.DEBUG)
+format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch = logging.StreamHandler(sys.stdout)
+ch.setFormatter(format)
+logger.addHandler(ch)
+"""Set up local logger End"""
+
+
+class SitePairsTest(object):
+
+    def __init__(self):
+        pass
